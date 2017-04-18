cbd86aeff6eadeb838bdea7facfd29510ada28c8
Minh Nguyen <nguyenminhnaru@gmail.com>
2017-04-17 23:33:19 -0400

    Add desgin.html

--- v.2/javascripts/main.js
+++ v.2/javascripts/main.js
@@ -30,109 +30,71 @@ function generateMap(dataNatality, elementID) {
         token: 'pk.eyJ1IjoibmFydW1pbmgiLCJhIjoiY2oxY3dvanlrMDAwdTJ3bzE5bnl4Mmk1ZyJ9.694Vl0Mc9dqbPrkL0svBhQ'
     });
     worldmap.addTo(map);
-
-    // // draw US map
-    // var myStyle_us = {
-    //     "color": "#ffffff",
-    //     "fillColor": "#ffffff",
-    //     "weight": 1,
-    //     "opacity": 0.65,
-    //     "fillOpacity": 0
-    // };
-
-    // L.geoJSON(ussData, {
-    //     style: myStyle_us
-    // }).addTo(map);
-
-    var color = d3.scaleSequential(d3.interpolateReds);
-
-    // var dataNatality = data;
+    map.addLayer(worldmap);
 
     // var myLayer_1 = L.geoJSON().addTo(map);
     // myLayer_1.addData(dataNatality);
 
     // // Original GEOJSON style
-    // var myStyle = {
-    //     "color": "#ffffff",
-    //     "fillColor": "#ffffff",
-    //     "weight": 1,
-    //     "opacity": 0.65,
-    //     "fillOpacity": 0
-    // };
 
-    // combine data
+    // calculate total birth
+    var totalBirth = _(dataNatality)
+        .groupBy('State')
+        .map((v, k) => ({
+            State: k,
+            Births: _.sumBy(v, 'Births')
+        })).value();
+
+    // var maxBirth = _.maxBy(totalBirth, 'Births');
+
+    // combine total birth data
+    _.each(ussData.features, function(d) {
+        d.properties.total_birth = {};
+        var state = d.properties.name;
+        _.each(totalBirth, function(n) {
+            if (n.State == state) {
+                d.properties.total_birth = n["Births"];
+            }
+        });
+    });
+
+    // combine natality data
     _.each(ussData.features, function(d) {
         d.properties.natalities = [];
         var state = d.properties.name;
         _.each(dataNatality, function(n) {
             if (n.State == state) {
                 var tmp = {
+                    "Census Region": n["Census Region"],
                     "Year": n["Year"],
-                    "Diabetes": n["Diabetes"],
-                    "Tobacco Use": n["Tobacco Use"],
-                    "Age of Mother": n["Age of Mother"],
-                    "Births": n["Births"]
+                    "Births": n["Births"],
+                    "Total Population": n["Total Population"],
+                    "Birth Rate": n["Birth Rate"],
+                    "Female Population": n["Female Population"],
+                    "Fertility Rate": n["Fertility Rate"]
                 };
                 d.properties.natalities.push(tmp);
             }
         });
     });
 
-    // _.each(ussData.features, function(d) {
-    //     d.properties.total = [];
-    //     var
-    // });
-
-    // var ussd = ussData.features;
-    // var ussf = _.map(ussd, function(d) { return d.properties.natalities; });
-    // console.log(ussf);
-
-    // _.each(ussData.features, function(d) {
-    //     d.properties.total = [];
-    //     _.each(ussData.features, function(n) {
-    //         var sum = _.sumBy(ussData.features, function(s) { return s.properties.natalities.Births});
-    //         var tmp = {
-    //             "Total": sum
-    //         };
-    //         d.properties.natalities.push(tmp);
-    //     });
-    // });
-
-    console.log(dataNatality);
-
-
-
-    // function getColor(d) {
-    //     return d > 1000 ? '#800026' :
-    //            d > 500  ? '#BD0026' :
-    //            d > 200  ? '#E31A1C' :
-    //            d > 100  ? '#FC4E2A' :
-    //            d > 50   ? '#FD8D3C' :
-    //            d > 20   ? '#FEB24C' :
-    //            d > 10   ? '#FED976' :
-    //                       '#FFEDA0';
-    // }
+    console.log(ussData);
 
     function getColor(d) {
-        return d > 90 ? '#800026' :
-               d > 80  ? '#BD0026' :
-               d > 70  ? '#E31A1C' :
-               d > 50  ? '#FC4E2A' :
-               d > 40   ? '#FD8D3C' :
-               d > 30   ? '#FEB24C' :
-               d > 20   ? '#FED976' :
-                          '#FFEDA0';
+        return d > 4000000 ? '#800026' :
+               d > 2500000 ? '#BD0026' :
+               d > 1000000 ? '#E31A1C' :
+               d > 500000  ? '#FC4E2A' :
+               d > 300000  ? '#FD8D3C' :
+               d > 200000  ? '#FEB24C' :
+               d > 100000  ? '#FED976' :
+                             '#FFEDA0';
     }
 
-    // var ussMap = ussData.features;
-    // var ahihi = _.map(ussMap, function(d) { return d.properties.name; })
-    // console.log(ahihi);
-
-    function myStyle(feature) {
+    // original GEOJSON style
+    function myStyle(d) {
         return {
-            fillColor: getColor(feature.properties.density),
-            // fillColor: getColor(testi[feature.properties.name]),
-            // fillColor: _.map(ussMap, function(d){return (testi[feature.properties.name] == undefined) ? "#fff" : getColor(testi[feature.properties.name]);}),
+            fillColor: getColor(d.properties.total_birth),
             weight: 1,
             opacity: 0.65,
             color: 'white',
@@ -140,53 +102,57 @@ function generateMap(dataNatality, elementID) {
         };
     }
 
-    L.geoJSON(ussData, {
-        style: myStyle
+    // Function Highlight feature on mouseover
+    function highlightFeature(e) {
+        var layer = e.target;
+
+        layer.setStyle({
+            weight: 4,
+            color: '#666',
+            opacity: 1,
+            fillColor: '#4286f4',
+            fillOpacity: 0.7
+        });
+
+        if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+            layer.bringToFront();
+        }
+    }
+
+    var featureLayer;
+
+    // Function on mouseout - reset layer style to its default
+    function resetHighlight(e) {
+        featureLayer.resetStyle(e.target);
+    }
+
+    // Click and zoom to state
+    function zoomToFeature(e) {
+        map.fitBounds(e.target.getBounds());
+    }
+
+    // Add the listeners on state layers
+    function onEachFeature(feature, layer) {
+        layer.on({
+            mouseover: highlightFeature,
+            mouseout: resetHighlight,
+            click: zoomToFeature
+        });
+    }
+
+    function onEachFeature(feature, layer) {
+        layer.on('mouseover', function(e) {
+            highlightFeature(layer);
+        });
+        layer.on('mouseout', function(e) {
+            resetHighlight(layer);
+        });
+    }
+
+    featureLayer = L.geoJson(ussData, {
+        style: myStyle,
+        onEachFeature: onEachFeature
     }).addTo(map);
-
-
-
-    // // Function Highlight feature on mouseover
-    // function highlightFeature(e) {
-    //     var layer = e.target;
-
-    //     layer.setStyle({
-    //         weight: 4,
-    //         color: '#666',
-    //         dashArray: '',
-    //         fillOpacity: 0.7
-    //     });
-
-    //     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-    //         layer.bringToFront();
-    //     }
-    // }
-
-    // var geojson;
-
-    // // Function on mouseout - reset layer style to its default
-    // function resetHighlight(e) {
-    //     geojson.resetStyle(e.target);
-    // }
-
-    // // Click and zoom to state
-    // function zoomToFeature(e) {
-    //     map.fitBounds(e.target.getBounds());
-    // }
-
-    // // Add the listeners on state layers
-    // function onEachFeature(feature, layer) {
-    //     layer.on({
-    //         mouseover: highlightFeature,
-    //         mouseout: resetHighlight,
-    //         click: zoomToFeature
-    //     });
-    // }
-
-    // geojson = L.geoJson(statesData, {
-    //     style: myStyle,
-    //     onEachFeature: onEachFeature
-    // }).addTo(map);
 
 }
 
